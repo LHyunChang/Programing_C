6f645d4bf1d74755db8948d4fdf6597c5118e6f1
LHyunChang <ckdqwqw@naver.com>
2023-12-06 18:22:10 +0900

    12-06_C_Programing

--- Chapter13_03/Chapter13_03/Chapter13_03.cpp
+++ Chapter13_03/Chapter13_03/Chapter13_03.cpp
@@ -12,5 +12,14 @@ int main()
 	str1[0] = 'X';
 	/*str2[0] = 'X';*/
 	printf("%s\n%s\n", str1, str2);
+
+	int num1 = 10;
+	int num2 = 20;
+	int num3 = 30;
+
+	int* arr[3] = { &num1, &num2, &num3 };
+
+	printf("%d %d %d\n", *arr[0], *arr[1], *arr[3]);
+
 	return 0;
 }
